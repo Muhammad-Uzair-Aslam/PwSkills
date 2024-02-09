a3cf2cc11bfa43d882423b886a46578b1505ae7e
Uzair Aslam <uzairaslam990@gmail.com>
2024-02-09 04:09:34 -0800

    navbar

--- app/(components)/navbar/navbar.js
+++ app/(components)/navbar/navbar.js
@@ -1,59 +1,32 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
 
-import Link from 'next/link';
-import Logo from '../logo/logo';
-
-const Navbar = ({ link1, link2, link3, link1To, link2To, link3To }) => {
-    return (
-        <div className='py-5 flex flex-col items-between space-y-9 bg-black px-8 mx-auto text-slate-200'>
-            <nav className='flex items-center justify-between'>
-                <Logo />
-                
-                <div className='hidden md:flex items-center space-x-9'>
-                    <Link href={link1To}>
-                        {link1}
-                    </Link>
-                    <Link href={link2To}>
-                        {link2}
-                    </Link>
-                    <Link href={link3To}>
-                        {link3}
-                    </Link>
-                </div>
-                <button
-                    onClick={() => {
-                        document.querySelector('.mobile-menu').classList.toggle('hidden');
-                    }}
-                    className='md:hidden flex items-center'
-                >
-                    <svg
-                        xmlns='http://www.w3.org/2000/svg'
-                        className='h-6 w-6'
-                        fill='none'
-                        viewBox='0 0 24 24'
-                        stroke='currentColor'
-                        strokeWidth={2}
-                    >
-                        <path
-                            strokeLinecap='round'
-                            strokeLinejoin='round'
-                            d='M4 6h16M4 12h16m-7 6h7'
-                        />
-                    </svg>
-                </button>
-            </nav>
-            <div className='mobile-menu hidden md:hidden flex flex-col items-end space-y-2 px-9 pt-3 pb-3 text-sm'>
-                <Link href={link1To}>
-                    {link1}
-                </Link>
-                <Link href={link2To}>
-                    {link2}
-                </Link>
-                <Link href={link3To}>
-                    {link3}
-                </Link>
+export default function Navbar() {
+  return (
+    <div className="">
+      <div className=" flex flex-row items-center h-24 ">
+        <div className=" flex  w-[15%] items-center">
+            <Image className="rounded-full md:ml-12 ml-5" src="/pw.jpg" width={40} height={30}/>
+            <p className="font-bold text-xl ml-1">SKILLS</p>
+        </div>
+        <div className="hidden md:block  w-[70%]  border-gray-100 border-2">
+            <div className="flex mx-4 my-3">
+        <svg className=" w-6 h-6  text-gray-650" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M464 428L339.92 303.9C359.916 276.421 370.673 243.304 370.64 209.32C370.64 120.37 298.27 48 209.32 48C120.37 48 48 120.37 48 209.32C48 298.27 120.37 370.64 209.32 370.64C243.304 370.673 276.421 359.916 303.9 339.92L428 464L464 428ZM209.32 319.69C187.488 319.692 166.146 313.22 147.992 301.092C129.839 288.964 115.69 271.725 107.334 251.555C98.9785 231.385 96.7917 209.191 101.05 187.778C105.309 166.366 115.822 146.697 131.26 131.26C146.697 115.822 166.366 105.309 187.778 101.05C209.191 96.7917 231.385 98.9785 251.555 107.334C271.725 115.69 288.964 129.839 301.092 147.992C313.22 166.146 319.692 187.488 319.69 209.32C319.656 238.581 308.016 266.634 287.325 287.325C266.634 308.016 238.581 319.656 209.32 319.69Z" fill="currentColor"></path></svg>
+            <input className="w-full hidden md:block outline-none "  placeholder="Search by product title"/>
             </div>
         </div>
-    );
-};
+        <div className="bg-orange-700 backdrop-saturate-125 hover:bg-orange-800 text-base  md:mx-5 px-3 py-1 hidden md:block rounded-lg text-white">
+          <button className="p-2">Login/Register</button>
+        </div>
+        <div className="md:hidden w-[85%] flex justify-end items-center ">
+        <svg className=" w-6 mx-2 h-6  text-gray-650" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M464 428L339.92 303.9C359.916 276.421 370.673 243.304 370.64 209.32C370.64 120.37 298.27 48 209.32 48C120.37 48 48 120.37 48 209.32C48 298.27 120.37 370.64 209.32 370.64C243.304 370.673 276.421 359.916 303.9 339.92L428 464L464 428ZM209.32 319.69C187.488 319.692 166.146 313.22 147.992 301.092C129.839 288.964 115.69 271.725 107.334 251.555C98.9785 231.385 96.7917 209.191 101.05 187.778C105.309 166.366 115.822 146.697 131.26 131.26C146.697 115.822 166.366 105.309 187.778 101.05C209.191 96.7917 231.385 98.9785 251.555 107.334C271.725 115.69 288.964 129.839 301.092 147.992C313.22 166.146 319.692 187.488 319.69 209.32C319.656 238.581 308.016 266.634 287.325 287.325C266.634 308.016 238.581 319.656 209.32 319.69Z" fill="currentColor"></path></svg>
+        <button className="mx-2 text-3xl">&#8801;</button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-export default Navbar;
+
+
--- app/page.js
+++ app/page.js
@@ -1,32 +1,10 @@
 import Image from "next/image";
+import Navbar from "./(components)/navbar/navbar";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-indigo-100 h-16 w-full flex justify-between px-4 md:px-10  md:items-center">
-        <div className="text-indigo-700 font-bold text-xl flex items-center">PW-Skills</div>
-        <div>
-          <ul className="flex flex-col md:flex-row items-start justify-end">
-            <li className="mx-2 cursor-pointer md:block">Home</li>
-            <li className="mx-2 cursor-pointer md:block">About US</li>
-            <li className="mx-2 cursor-pointer md:block">Contact Us</li>
-            <li className="mx-2 cursor-pointer md:block">Blog</li>
-          </ul>
-        </div>
-        <div className="bg-indigo-800 px-3 py-1 hidden md:block rounded-lg text-white font-bold">
-          <button>Login/Signup</button>
-        </div>
-        {/* <div className="md:hidden">#&8801</div> */}
-      </div>
-      <div className="flex items-center justify-center h-screen ">
-        <div className="border  border-gray-300 p-4 flex flex-col md:items-center md:flex-row rounded-lg">
-          <Image className="md:rounded-full md:w-20 md:h-20 h-50  md:mx-2" src="/uzair.JPG" width={200} height={50} />
-
-          <div className="mx-2">
-            <p className="font-bold mt-2">Muhammad Uzair Aslam</p>
-            <p className="text-gray-500 mt-2 text-sm">Web Developer</p></div>
-        </div>
-      </div>
+      <Navbar/>
     </>
   );
 }
