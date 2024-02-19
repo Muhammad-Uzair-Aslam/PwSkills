d394af85e42006b682dc4b3ec66f6bf8f652810e
Uzair Aslam <uzairaslam990@gmail.com>
2024-02-19 07:38:22 -0800

    card hovering changes

--- app/(components)/categoriesCard/categoriesCard.js
+++ app/(components)/categoriesCard/categoriesCard.js
@@ -3,20 +3,20 @@ import React from 'react'
 
 export default function CategoriesCard() {
     return (
-        <div className='flex flex-col rounded-md shadow-md '>
-            <div className='p-5 flex flex-row bg-slate-50'>
+        <div className='flex flex-col rounded-lg shadow-lg group'>
+            <div className=' p-2 md:p-5 flex flex-row rounded-t-lg justify-between bg-slate-50 group-hover:bg-gradient-to-r from-slate-50 to-purple-200'>
                 <div>
                     <h1 className='text-md font-bold'>Data Science & Analytics</h1>
                     <p className='text-sm pt-3'>Become expert at drawing intelligence from data and get...</p>
                 </div>
-                <div>
-                    <Image src={"/ai.svg"} width={50} height={50} />
+                <div className='flex justify-center items-center'>
+                    <Image src={"/ai.svg"} width={80} height={80} />
                 </div>
 
             </div>
-            <div className='p-5 flex flex-row justify-between'>
+            <div className='p-3 md:p-5 flex flex-row justify-between '>
                 <div>
-                    <p>Know More</p>
+                    <p className='text-sm md:text-md '>Know More</p>
                 </div>
                 <div className='flex justify-center items-center '>
                     <svg
