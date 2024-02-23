d73f747fd7c759353ec6ae814ae3676c1d6c940d
Uzair Aslam <uzairaslam990@gmail.com>
2024-02-23 04:44:17 -0800

    slider

--- app/(components)/ImageSlider/ImageSlider.js
+++ app/(components)/ImageSlider/ImageSlider.js
@@ -5,11 +5,12 @@ import React, { useState } from 'react';
 const Slider = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const cards = [
-    { id: 1, title: 'Card 1',url:"/datascience.jpeg",content:"" },
-    { id: 2, title: 'Card 2',url:"/dataAnalytics.jpeg",content:"" },
-    { id: 3, title: 'Card 3',url:"/Hinglish.jpeg",content:"" },
-    { id: 4, title: 'Card 4',url:"/artificialIntelligence.jpeg",content:"" },
-    { id: 5, title: 'Card 5',url:"/generativeAI.jpeg",content:"" },
+    { id: 1, title: 'Data Science With Generative AI Course',url:"/datascience.jpeg",content:"Job Assistance | Ticket to fast-tracked career growth in Data Science" },
+    { id: 2, title: 'Data Analytics Course',url:"/dataAnalytics.jpeg",content:"Job Assistance | Gain Data Analytics Expertise" },
+    { id: 3, title: 'Data Analytics Course Hinglish',url:"/Hinglish.jpeg",content:"Job Assistance | Gain Data Analytics Expertise" },
+    { id: 4, title: 'Mastering the Power of Artificial Intelligence Course',url:"/artificialIntelligence.jpeg",content:"Learn Generative AI fundamentals and Applications. Ideal for AI enthusiasts" },
+    { id: 5, title: 'Data Science with Generative AI Course-Hinglish',url:"/generativeAI.jpeg",content:"Job Assistance | Ticket to fast-tracked career growth in Data Science" },
+    { id: 2, title: 'Data Analytics Course',url:"/dataAnalytics.jpeg",content:"Job Assistance | Gain Data Analytics Expertise" },
   ];
 
   const prevSlide = () => {
@@ -21,21 +22,39 @@ const Slider = () => {
   };
 
   return (
-    <div className="relative w-full max-w-4xl mx-auto">
+    <div className="relative w-full mt-10">
       <div className="overflow-hidden">
         <div
-          className="flex transition-transform duration-300"
+          className="flex transition-transform duration-300 "
           style={{ transform: `translateX(-${currentIndex * 100}%)` }}
         >
           {cards.map((card) => (
             <div
               key={card.id}
-              className="w-full lg:w-1/3 flex-shrink-0 px-2"
+              className=" mx-auto w-full bg-white lg:w-[380px] rounded-lg flex-shrink-0 lg:mx-2"
             >
-              <div className="bg-blue-500 text-white p-10 rounded-lg text-center">
+              <div className=''>
+              <Image className='w-full rounded-t-lg' src={card.url} width={400} height={400}/>
+              </div>
+              <div className="p-6 md:p-3 text-center lg:text-left font-semibold text-gray-800 text-lg">
                 {card.title}
               </div>
-              <Image src={card.url} width={400} height={400}/>
+              <div className="px-6 pb-5 text-center md:px-3 lg:text-left text-sm text-gray-800">
+                {card.content}
+              </div>
+              <div>
+                <h1 className="p-3 font-semibold text-sm text-gray-800">
+                  Registration Started
+                </h1>
+              </div>
+              <div className='flex gap-2 w-full'>
+                <div className='rounded-lg w-1/2 bg-orange-700 mx-3 text-center hover:bg-orange-800 text-white'>
+                  <button className='px-3 py-2'>Explore</button>
+                </div>
+                <div className='w-1/2 rounded-lg border border-1 mx-3 text-center border-orange-700 text-orange-700  '>
+                  <button className='px-3 py-2'>Buy Now</button>
+                </div>
+              </div>
             </div>
           ))}
         </div>
